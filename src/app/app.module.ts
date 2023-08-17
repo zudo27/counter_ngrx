feec051ea3788faafeb54028d1320480fd9cb7b4
deepika <deepi0444@gmail.com>
2023-08-17 13:12:03 +0530

    counter app using ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { CounterButtonsComponent } from './components/counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './components/counter-output/counter-output.component';
+import { StoreModule } from "@ngrx/store";
+import { counterReducer } from "./state/counter.reducer";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterButtonsComponent,
+    CounterOutputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({counter: counterReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
